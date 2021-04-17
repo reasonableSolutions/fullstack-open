de8dcc138780032539043896ebba01f56527cd27
Ka Ming Wong <wg.kaming@gmail.com>
2021-04-16 22:15:07 -0700

    Exercise 1.6 complete

--- part1/example/part1/src/App.js
+++ part1/example/part1/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Hello = () => (      
-    <>      
-      <p>Hello world</p>    
-    </>  
-)
-
-const App = () => (
-  <>
-    <p>Greetings</p>
-    <Hello />
-    <Hello />
-  </>
-)
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {    
+    setAll(allClicks.concat('L'))    
+    setLeft(left + 1)  
+  }
+  const handleRightClick = () => {    
+    setAll(allClicks.concat('R'))    
+    setRight(right + 1)  
+  }
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>    </div>
+  )
+}
 
 export default App
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodHandler = () => {
+    setGood(good+1)
+  }
+
+  const neutralHandler = () => {
+    setNeutral(neutral+1)
+  }
+
+  const badHandler = () => {
+    setBad(bad+1)
+  }
+
+  return (
+    <div>
+      {
+        /*
+        <Heading text="give feedback"/>
+        <Button /><Button /><Button />
+        <Heading text = "statistics" />
+        <Stat />
+        <Stat />
+        <Stat />
+        */
+      }
+      <Heading text="give feedback" />
+      <Button handler={goodHandler} text="good" /><Button handler={neutralHandler} text="neutral" /><Button handler={badHandler} text="bad" />
+      <Heading text = "statistics" />
+      <Stat label="good" number={good} />
+      <Stat label="neutral" number={neutral} />
+      <Stat label="bad" number={bad} />
+    </div>
+  )
+}
+
+const Heading = (props) => {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handler}>{props.text}</button>
+  )
+}
+
+const Stat = (props) => {
+  return (
+    <p>{props.label} {props.number}</p>
+  )
+}
+
+export default App
