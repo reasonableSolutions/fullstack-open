bd75c60f9bf2ea74a434bc66b34e9f11ef85ead7
Ka Ming Wong <wg.kaming@gmail.com>
2021-04-22 21:28:20 -0700

    Exercise 2.1 correction, accessing arbitrary # of parts

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -59,9 +59,9 @@ const Part = (props) => {
 const Content = ({ course }) => {
   return (
     <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      {course.parts.map(part =>
+        <Part key={part.id} part={part} />
+        )}
     </div>
   )
 }
