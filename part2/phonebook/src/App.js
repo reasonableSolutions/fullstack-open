0ad2b6fed9a560da655bb02ba5c3e1c473a219d5
Ka Ming Wong <wg.kaming@gmail.com>
2021-04-25 23:17:56 -0700

    Exercise 2.9 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,20 @@ import React, { useState } from 'react'
 
 const App = () => {
   
-  const [ persons, setPersons ] = useState([
-    { 
-      name: 'Arto Hellas', 
-      number: '852-1216654'
-    }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ nameFilter, setNameFilter ] = useState('')
+
+  const personsToShow = (nameFilter === '')
+    ? persons
+    : persons.filter(person => 
+      person.name.match(RegExp(nameFilter,'gi')) !== null)
 
   const handleAdd = (event) => {
     event.preventDefault()
@@ -34,9 +40,15 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setNameFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Filter shown by: <input value={nameFilter} onChange={handleFilter}/></div>
+      <h2>Add a contact</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleTypingName}/>
@@ -50,7 +62,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
+        {personsToShow.map(person => 
           <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
