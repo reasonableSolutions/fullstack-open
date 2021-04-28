6e8cb592bef83c7e46d7eacdd5fb3db78aedaabb
Ka Ming Wong <wg.kaming@gmail.com>
2021-04-28 00:24:45 -0700

    Exercise 2.11 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import InputForm from './components/InputForm'
 import Output from './components/Output'
 
 const App = () => {
   
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ nameFilter, setNameFilter ] = useState('')
@@ -46,6 +42,15 @@ const App = () => {
   const handleFilter = (event) => {
     setNameFilter(event.target.value)
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        const notes = response.data
+        setPersons(notes)
+      })
+  }, [])
 
   return (
     <div>
