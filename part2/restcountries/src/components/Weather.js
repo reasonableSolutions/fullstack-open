4b9d7e432756da5dd0a8db26fbbc4575a8cc02f6
Ka Ming Wong <wg.kaming@gmail.com>
2021-05-04 22:36:10 -0700

    Exercise 2.14 complete (FINALLY!!!)

--- part2/restcountries/src/components/Weather.js
+++ part2/restcountries/src/components/Weather.js
@@ -1,45 +1,39 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-const Weather = ({name, capital, key}) => {
-    const api_key = process.env.REACT_APP_API_KEY
-    const [weather, setWeather] = useState({})
+const Weather = ({name, capital}) => {
+    const [weather, setWeather] = useState(null)
+
+    console.log(process.env.REACT_APP_API_KEY)
 
     useEffect(() => {
-        let URL = 'http://api.weatherstack.com/current?access_key=' +
-            api_key +
-            '&query=' +
-            capital +
-            ',' +
-            name
-          console.log(URL)
-          axios
-            .get(URL)
-            .then(response => 
-              setWeather(response.data))
-      }, [])
+        axios
+        .get('http://api.weatherstack.com/current', {
+            params: {
+                access_key: process.env.REACT_APP_API_KEY,
+                query: `${capital}, ${name}`
+            }
+        })
+        .then(response => 
+            setWeather(response.data))
+    }, [capital, name])
 
-    console.log(
-        `weather, 
-        ${api_key}
-        ${name}
-        ${capital}`
-    )
-    
-    if (weather === undefined){
+    console.log('weather ', weather)
+
+    if (weather){
         return (
-            <>
-            </>
+            <div>
+                <h1>Current weather in {capital}</h1>
+                <div><b>Temperature:</b> {weather.current.temperature} Celsius</div>
+                <img src={weather.current.weather_icons[0]} alt=""></img>
+                <div>{weather.current.weather_descriptions[0]}</div>
+                <div><b>Wind:</b> {weather.current.wind_speed} mph {weather.current.wind_dir}</div>
+            </div>
         )
     }
     else return (
         <div>
-                <h1>Current weather in {capital}</h1>
-                <div><b>Temperature:</b> {weather.current.temperature} Celsius</div>
-                <img src={weather.current.weather_icons[0]}></img>
-                <div>{weather.current.weather_descriptions[0]}</div>
-                <div><b>Wind:</b> {weather.current.wind_speed} mph {weather.current.wind_dir}</div>
-            </div>
+        </div>
     )
 }
 
