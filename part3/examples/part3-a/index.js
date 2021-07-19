4f76fe9ed20b7ef0c44335ef2de316967b09a2cc
Ka Ming Wong <wg.kaming@gmail.com>
2021-07-19 19:00:14 -0400

    Got to 3-1 exercises

--- part3/examples/part3-a/index.js
+++ part3/examples/part3-a/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [  
   {    
     id: 1,    
@@ -23,11 +25,51 @@ let notes = [
 ]
 
 app.get('/', (request, response) => {  
-  response.send('<h1>Hello world!</h1>')
+  response.send('<h1>What is test test test</h1>')
 })
+
 app.get('/api/notes', (request, response) => {  
   response.json(notes)
 })
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  //console.log(id)
+  const note = notes.find(note => note.id === id)
+  // console.log(note)
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes.concat(note)
+  response.json(note)
+})
+
+const generateId = () => {
+  const MaxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  return MaxId + 1
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
