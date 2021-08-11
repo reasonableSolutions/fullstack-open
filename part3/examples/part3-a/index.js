b66a11e328d839fa47d93daef126b7911b762e53
Ka Ming Wong <wg.kaming@gmail.com>
2021-08-10 18:04:32 -0700

    Preparing to deploy to Heroku

--- part3/examples/part3-a/index.js
+++ part3/examples/part3-a/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-
 app.use(express.json())
+
+const cors = require('cors')
+app.use(cors())
 
 let notes = [  
   {    
@@ -87,7 +89,7 @@ const unknownEndpoint = (request, response, next) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
